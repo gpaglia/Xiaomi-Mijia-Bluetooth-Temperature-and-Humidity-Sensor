c02d49b0cf175f657ebb470eb70f98cbbff21009
Gian Enrico Paglia <gianenrico.paglia@gmail.com>
2020-10-01 17:46:05 +0200

    new example with NativeByteBuffer

--- java/jni/exp/callc.c
+++ java/jni/exp/callc.c
@@ -9,45 +9,78 @@
  * Signature: (Lcom/gpaglia/bluetooth/exp/test/Another;)I
  */
 JNIEXPORT jint JNICALL Java_com_gpaglia_bluetooth_exp_test_JniExample1_callc
-  (JNIEnv * env, jobject this, jobject cb, jint val) {
-    printf("Entering call.c --- val=%d\n", val);
-    jclass cbClass = (* env) -> GetObjectClass(env, cb);
-    jmethodID cbMethod = (* env) -> GetMethodID(env, cbClass, "callback", "(I)V");
+  (JNIEnv *env, jobject this, jobject cb, jint val)
+{
+  printf("Entering call.c --- val=%d\n", val);
+  jclass cbClass = (*env)->GetObjectClass(env, cb);
+  jmethodID cbMethod = (*env)->GetMethodID(env, cbClass, "callback", "(I)V");
 
-    printf("In call.c --- found method? %d\n", (cbMethod != 0));
+  printf("In call.c --- found method? %d\n", (cbMethod != 0));
 
-    (* env) -> CallVoidMethod(env, cb, cbMethod, 1000 + val);
+  (*env)->CallVoidMethod(env, cb, cbMethod, 1000 + val);
 
-    printf("In call.c --- returning %d\n", 10000 + val);
+  printf("In call.c --- returning %d\n", 10000 + val);
 
-    return 10000 + val;
-  }
+  return 10000 + val;
+}
 
 JNIEXPORT jint JNICALL Java_com_gpaglia_bluetooth_exp_test_ThreadExample_00024Runner_loop
-  (JNIEnv * env, jobject this, jobject cb, jint cnt, jint st) {
-    printf("Entering call.c --- cnt=%d, st=%d\n", cnt, st);
-    if (st < 0) {
-      st = 1;
-      printf("In call.c -- setting st to 1 by default\n");
-    }
-    jclass cbClass = (* env) -> GetObjectClass(env, cb);
-    jmethodID cbMethod = (* env) -> GetMethodID(env, cbClass, "callback", "(I)V");
-
-    printf("In call.c --- found method? %d\n", (cbMethod != 0));
-
-    size_t act = 0;
-    for (size_t i = 0; i < cnt; i++)
-    {
-      (* env) -> CallVoidMethod(env, cb, cbMethod, 1000 + i);
-
-      printf("In call.c --- sleeping for %d secs\n", st);
-
-      act = i + 1;
-      
-      sleep(st);
-
-    }
-    
-    printf("In call.c -- finished loop, act=%d, returning=%d\n", act, act + 10000);
-    return 10000 + act;
+  (JNIEnv *env, jobject this, jobject cb, jint cnt, jint st)
+{
+  printf("Entering call.c --- cnt=%d, st=%d\n", cnt, st);
+  if (st < 0)
+  {
+    st = 1;
+    printf("In call.c -- setting st to 1 by default\n");
   }
+  jclass cbClass = (*env)->GetObjectClass(env, cb);
+  jmethodID cbMethod = (*env)->GetMethodID(env, cbClass, "callback", "(I)V");
+
+  printf("In call.c --- found method? %d\n", (cbMethod != 0));
+
+  size_t act = 0;
+  for (int i = 0; i < cnt; i++)
+  {
+    (*env)->CallVoidMethod(env, cb, cbMethod, 1000 + i);
+
+    printf("In call.c --- sleeping for %d secs\n", st);
+
+    act = i + 1;
+
+    sleep(st);
+  }
+
+  printf("In call.c -- finished loop, act=%d, returning=%d\n", act, act + 10000);
+  return 10000 + act;
+}
+
+/*
+ * Class:     com_gpaglia_bluetooth_exp_test_JniExample2
+ * Method:    callWithByteBuffer
+ * Signature: (IS)Ljava/nio/ByteBuffer;
+ */
+JNIEXPORT jobject JNICALL Java_com_gpaglia_bluetooth_exp_test_JniExample2_callWithByteBuffer
+  (JNIEnv * env, jobject this, jint ival, jshort shval)
+{
+    static struct MyNativeStruct {
+      int iv;
+      short sv;
+    } s;
+
+
+    printf(
+      "Entering callWithByteBuffer --- ival=%d (size: %d), shval=%d (size: %d) structSize=%d\n", 
+      ival, 
+      sizeof(ival),
+      shval,
+      sizeof(shval),
+      sizeof(struct MyNativeStruct)
+    );
+
+    s.iv = ival + 1000;
+    s.sv = shval + 2000; 
+
+    jobject bb = (*env)->NewDirectByteBuffer(env, (void *) &s, sizeof(struct MyNativeStruct));
+
+    return bb;
+}
---
+++ java/src/com/gpaglia/bluetooth/exp/test/JniExample2.java
@@ -0,0 +1,27 @@
+package com.gpaglia.bluetooth.exp.test;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class JniExample2 {
+  
+  private JniExample2() {}
+
+  public static void main(String[] args) {
+    System.loadLibrary("jniexp");
+    final JniExample2 example = new JniExample2();
+    
+    final ByteBuffer bb = example.callWithByteBuffer(100, (short) 200);
+    bb.order(ByteOrder.nativeOrder());
+    System.out.printf("Result from jni is %d %d %n", bb.getInt(0), bb.getShort(4));
+
+    System.out.print("Buffed content: [");
+    for (int i = 0; i < bb.limit(); i++) {
+      System.out.printf("%02x ", bb.get(i));
+    }
+    System.out.print("]\n");
+}
+
+private native ByteBuffer callWithByteBuffer(final int ival, final short shval);
+
+}
